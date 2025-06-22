1def3b041f2d306783637195005ec12064d89078
KARRI SAI MANIKANTA REDDY <144449541+Manikantareddy4567@users.noreply.github.com>
2025-06-22 15:17:03 +0530

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,7 @@
+TRAINING_PATH = "./data/training"
+TESTING_PATH = "./data/testing"
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 8
+EPOCHS = 20
+CLASS_NAMES = ["glioma", "meningioma", "notumor", "pituitary"]
+NUM_CLASSES = len(CLASS_NAMES)
